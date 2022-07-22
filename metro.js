b4a65be0a063368aa397f2dfc44e7f3bce5ab00f
sunidhikumari-jft <sunidhi.kumari@jellyfishtechnologies.com>
2022-07-22 10:50:48 +0530

    updated file

--- metro.js
+++ metro.js
@@ -1,76 +1,91 @@
-var a, b;
+// var a, b;
 
 function selectOption()
 {
     let arr=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     for(let i=0; i<arr.length; i++)
     {
-        document.getElementById('Source').innerHTML+=`<option onclick="fun(${i})">${arr[i]}</option>`
-        document.getElementById('Destination').innerHTML+=`<option onclick="done(${i})">${arr[i]}</option>`
+        document.getElementById('Source').innerHTML+=`<option value=${i+1}>${arr[i]}</option>`
+        document.getElementById('Destination').innerHTML+=`<option value=${i+1}>${arr[i]}</option>`
     }
 }
 selectOption();
-function fun(data)
-{
-  a=arr[data];
-  console.log(a);
-}
-function done(data)
-{
-	b=arr[data];
-	console.log(b);
-}
+// function fun(data)
+// {
+//   a=arr[data];
+//   console.log(a);
+// }
+// function done(data)
+// {
+// 	b=arr[data];
+// 	console.log(b);
+// }
+
+
 
 
 
  function fareCalculation()
  {
-    let arr=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    let station=[];
-	for(let i=0; i<arr.length; i++)
-	{
-		station.push[i];
-		let cardpayment=false;
-		let source= 6;
-		let destination=8;
-		let sC=destination-source;//sC=station covered
+	let x=document.getElementById('Source').value;
+	let y=document.getElementById('Destination').value;
+	
+	let snumber=(Math.abs(Number(x-y)));
+
+	console.log(snumber)
+    // let arr=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    // let station=[];
+
+
+	// for(let i=0; i<arr.length; i++)
+	// {
+		// station.push[i];
+		// let cardpayment=false;
+		
+		// let source= 6;
+		// let destination=8;
+		// let sC=destination-source;//sC=station covered
 		let fare=0;
-		if(0<=sC<=4)
+		if(snumber>0 && snumber<=4)
 		{
 			fare=10;
 		}
-		 else if(5<=sC<=8)
+		 else if(snumber>4 && snumber<=8)
 		{
 			fare=20;
 		}
-		else if(9<=sC<=12)
+		else if(snumber>8 && snumber<=12)
 		{
 			fare=30;
 		}
-		else if(13<=sC<=16)
+		else if(snumber>12 && snumber<=16)
 		{
 			fare=40;
 		}
-		else if(17<=sC<=20)
+		else if(snumber>16 && snumber<=20)
 		{
 			fare=50;
 		}
-		else if(21<=sC<=24)
+		else if(snumber>20 && snumber<=24)
 		{
 			fare=60;
 		}
-		else (sC>24)
+		else if(snumber>24)
 		{
 			fare=70;
 		}
-		if(cardpayment)
-		{
-			fare=fare*10/100;
+		// if(cardpayment)
+		// {
+		// 	fare=fare*10/100;
+		// }
+		if(document.getElementById('Boxes').value==='Card payment' && snumber>=5){
+			fare=fare-(10/100*fare);
 		}
-	}
+	
 	console.log(fare);
             
-			document.getElementById('totalfare').innerHTML=fare;
+			document.getElementById('totalfare').value=fare;
     
- }
+	}
+
  fareCalculation();
